623b6a3d26c0065170b1ae362272adecb0d3a1e4
Guilherme Fornagiero de Carvalho <guifornagiero.c@hotmail.com>
2022-01-10 16:49:02 -0300

    Public readonly e PD
    Propriedades modificadas de private para public readonly e programação defensiva na propriedade Data

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,9 +1,9 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
 
-    adiciona(negociacao: Negociacao) {
+    adiciona(negociacao: Negociacao): void {
         this.negociacoes.push(negociacao);
     }
 
